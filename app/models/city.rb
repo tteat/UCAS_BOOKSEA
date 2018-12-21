6e90f947eff2ad7fc3a8341e4500dbbd1edc84fb
tangting <>
2018-12-21 17:10:24 +0800

    delete

--- app/models/city.rb
+++
@@ -1,9 +0,0 @@
-class City < ApplicationRecord
-  has_many :friends
-
-  # VALIDATION
-
-  validates :name, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-end
