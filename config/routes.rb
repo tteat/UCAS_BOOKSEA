1f893dcbccb29e324b1db3d16035de71c346e576
zhangjiecoco <934736683@qq.com>
2018-12-21 15:29:51 +0800

    register and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  resources :cities
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pages/index'
+
+  root 'pages#index'
+
+  devise_for :users, path: 'auth', path_names: {
+    sign_up: 'register',
+    sign_in: 'login',
+    sign_out: 'logout',
+    edit: 'account'
+  }
 end
